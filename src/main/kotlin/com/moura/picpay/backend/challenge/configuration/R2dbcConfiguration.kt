7acf1a3e9a71e03e451e1c591dc3dbfb751b050c
Ismael Moura <ismaeldesousa2014@outlook.com>
2024-07-21 18:29:24 -0300

    fix: enable r2dbc auditing

--- src/main/kotlin/com/moura/picpay/backend/challenge/configuration/R2dbcConfiguration.kt
+++ src/main/kotlin/com/moura/picpay/backend/challenge/configuration/R2dbcConfiguration.kt
@@ -5,10 +5,12 @@ import com.moura.picpay.backend.challenge.domain.user.UserId
 import io.r2dbc.spi.ConnectionFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions
 import org.springframework.data.r2dbc.dialect.DialectResolver
 
 @Configuration
+@EnableR2dbcAuditing
 class R2dbcConfiguration {
     @Bean
     fun r2dbcCustomConversions(connectionFactory: ConnectionFactory): R2dbcCustomConversions {
