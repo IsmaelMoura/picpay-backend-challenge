df314c1723dbe26578ea80da2569699e8de9d3b0
Ismael Moura <ismaeldesousa2014@outlook.com>
2024-08-19 21:24:56 -0300

    refactor: migrate to gradle version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
-    id("org.springframework.boot") version "3.3.1"
-    id("io.spring.dependency-management") version "1.1.6"
-    kotlin("jvm") version "2.0.0"
-    kotlin("plugin.spring") version "2.0.0"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
+    alias(libs.plugins.spring.boot) apply true
+    alias(libs.plugins.spring.dependency.management) apply true
+    alias(libs.plugins.kotlin.jvm) apply true
+    alias(libs.plugins.kotlin.spring) apply true
+    alias(libs.plugins.ktlint) apply true
 }
 
 group = "com.moura"
@@ -25,49 +25,42 @@ repositories {
     mavenCentral()
 }
 
-val kotlinLoggingJvmVersion: String by project
-val ulidjVersion: String by project
-val kotestVersion: String by project
-val apacheCommonsVersion: String by project
-val springMockkVersion: String by project
-
 dependencies {
     // Web
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation(libs.spring.webflux)
+    implementation(libs.jackson.kotlin)
 
     // Database
-    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
-    implementation("org.flywaydb:flyway-core")
-    implementation("org.flywaydb:flyway-database-postgresql")
-    runtimeOnly("org.postgresql:r2dbc-postgresql")
-    runtimeOnly("org.postgresql:postgresql")
+    implementation(libs.spring.r2dbc)
+    runtimeOnly(libs.flyway.postgres)
+    runtimeOnly(libs.postgres.driver.r2dbc)
+    runtimeOnly(libs.postgres.driver.jdbc)
 
     // Observability / Metrics
-    implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("io.micrometer:micrometer-tracing-bridge-brave")
-    implementation("io.micrometer:micrometer-registry-prometheus")
-    implementation("io.github.oshai:kotlin-logging:$kotlinLoggingJvmVersion")
+    implementation(libs.spring.actuator)
+    implementation(libs.micrometer.tracing.brave)
+    implementation(libs.micrometer.prometheus)
+    implementation(libs.kotlin.logging.jvm)
 
     // Kotlin
-    implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-slf4j")
+    implementation(libs.reactor.kotlin.extensions)
+    implementation(libs.kotlin.reflect)
+    implementation(libs.kotlin.coroutines.reactor)
+    implementation(libs.kotlin.coroutines.slf4j)
 
     // ULID
-    implementation("io.azam.ulidj:ulidj:$ulidjVersion")
+    implementation(libs.ulidj)
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.springframework.boot:spring-boot-testcontainers")
-    testImplementation("io.projectreactor:reactor-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.testcontainers:junit-jupiter")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:$kotestVersion")
-    testImplementation("org.apache.commons:commons-lang3:$apacheCommonsVersion")
-    testImplementation("com.ninja-squad:springmockk:$springMockkVersion")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation(libs.spring.test)
+    testImplementation(libs.spring.testcontainers)
+    testImplementation(libs.reactor.test)
+    testImplementation(libs.kotlin.test.junit)
+    testImplementation(libs.testcontainers.junit)
+    testImplementation(libs.kotlin.coroutines.test)
+    testImplementation(libs.kotest.junit)
+    testImplementation(libs.apache.commons)
+    testImplementation(libs.spring.mockk)
+    testImplementation(libs.junit.plataform)
 }
 
 kotlin {
