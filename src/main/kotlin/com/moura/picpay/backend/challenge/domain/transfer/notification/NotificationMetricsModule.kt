fdc01b2a7f0926b9f1d428e8b14464c7cec22936
Ismael Moura <ismaeldesousa2014@outlook.com>
2024-08-21 20:56:23 -0300

    feat: include 'failed' tag to transfer notification metric

--- src/main/kotlin/com/moura/picpay/backend/challenge/domain/transfer/notification/NotificationMetricsModule.kt
+++ src/main/kotlin/com/moura/picpay/backend/challenge/domain/transfer/notification/NotificationMetricsModule.kt
@@ -1,6 +1,8 @@
 package com.moura.picpay.backend.challenge.domain.transfer.notification
 
 import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tag
+import io.micrometer.core.instrument.Timer
 import org.springframework.stereotype.Component
 import kotlin.time.measureTimedValue
 import kotlin.time.toJavaDuration
@@ -12,17 +14,21 @@ class NotificationMetricsModule(
     suspend fun measureSendNotificationRequest(block: suspend () -> SendResult): SendResult {
         return measureTimedValue { block() }
             .also { timedValue ->
-                timedValue.value
-                    .onSuccess {
-                        registry
-                            .timer(NOTIFICATION_SENDING_TIMER)
-                            .record(timedValue.duration.toJavaDuration())
-                    }
+                Timer.builder(NOTIFICATION_SENDING_TIMER)
+                    .description("Duration of send transfer notification")
+                    .tags(
+                        setOf(
+                            Tag.of(SEND_FAILED_TAG, (timedValue.value is SendResult.Failure).toString()),
+                        ),
+                    )
+                    .register(registry)
+                    .record(timedValue.duration.toJavaDuration())
             }
             .value
     }
 
     companion object {
         private const val NOTIFICATION_SENDING_TIMER = "transfer.notification.duration"
+        private const val SEND_FAILED_TAG = "failed"
     }
 }
